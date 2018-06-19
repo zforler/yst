3ebdd8e2c25df64d68d47db9e46b4a7453dcc657
zfor <1517323360@qq.com>
2018-06-19 22:06:53 +0800

    201806192206

--- program/pages/custom/shopmanage/shopmanage.js
+++ program/pages/custom/shopmanage/shopmanage.js
@@ -8,5 +8,16 @@ new Page({
     itemClick: function(e){
         console.log(e)
         this.setData({ list: { [e.currentTarget.dataset.val]:'gd-item-detail'}})
-    }
+    },
+    addGoods: addGoods,
+    deleteGoods: deleteGoods
 })
+
+
+function addGoods(){
+
+}
+
+function deleteGoods(){
+    
+}
